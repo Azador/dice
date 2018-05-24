cc3bf22dc75951e3f513ddfb9bf8bc311c677519
Azador <github@lobo.mensa.uberspace.de>
2018-05-24 21:14:44 +0200

    List of images activated, loading and showing image implemented.

--- dice.py
+++ dice.py
@@ -156,56 +156,171 @@ class Trainer:
 
 class ImagesModel (QtGui.QStandardItemModel):
     
+    IMAGE_NAME, POS_X, POS_Y, VALUE, COLUMNS = range (5)
+    
     def __init__ (self):
-        QtGui.QStandardItemModel.__init__ (self, 5, 3)
-        self.data_list = ["Test"]
-        #self.setData (self.index (0, 0), "Test")
-        self.setData (self.index (0, 0), "Test")
-        #print (dir (self))
+        QtGui.QStandardItemModel.__init__ (self, 0, 4)
+        self.setDataList([])
         
     def setDataList (self, data):
         self.beginResetModel ()
         self.data_list = data
         self.endResetModel ()
+        self.setRowCount (len (data))
         self.dataChanged.emit (self.index (0,0), self.index (len (data)-1, 3))
     
     def columnCount (self, parent_index):
         if parent_index == QtCore.QModelIndex ():
-            return 3
+            return ImagesModel.COLUMNS
         
         return 0
     
     def headerData (self, section, orientation, role):
         if orientation == QtCore.Qt.Horizontal:
             if role == QtCore.Qt.DisplayRole:
-                if section == 0:
+                if section == ImagesModel.IMAGE_NAME:
                     return "Image"
                 
-                if section == 1:
-                    return "Pos"
+                elif section == ImagesModel.POS_X:
+                    return "X"
                 
-                return "Value"
-
-    def rowCount (self, parent_index):
-        if parent_index == QtCore.QModelIndex ():
-            return len (self.data_list)
-
-        return 0
-    
-    def data (self, index, role):
-        if role == QtCore.Qt.DisplayRole:
-            print ("index:", index.row (), index.column ())
-            #print ("args:", repr (args))
-            #print ("index", repr (index))
-            #print ("role ", repr (role))
-            return "Blub"
-        
+                elif section == ImagesModel.POS_Y:
+                    return "Y"
+                
+                elif section == ImagesModel.VALUE:
+                    return "Value"
+                
         return None
 
+    def data (self, index, role):
+        if index.row () < len (self.data_list):
+            row = index.row ()
+            col = index.column ()
+            if role == QtCore.Qt.DisplayRole:
+                if col == ImagesModel.IMAGE_NAME:
+                    return os.path.basename (self.data_list[row][0])
+                
+                elif col == ImagesModel.POS_X:
+                    if len (self.data_list[row]) >= 3:
+                        return "{}".format (self.data_list[row][2])
+                    else:
+                        return "---"
+                    
+                elif col == ImagesModel.POS_Y:
+                    if len (self.data_list[row]) >= 3:
+                        return "{}".format (self.data_list[row][3])
+                    else:
+                        return "---"
+                    
+                elif col == ImagesModel.VALUE:
+                    return "{}".format (self.data_list[row][1])
+                
+            elif role == QtCore.Qt.TextAlignmentRole:
+                if col == ImagesModel.IMAGE_NAME:
+                    return QtCore.Qt.AlignLeft
+                else:
+                    return QtCore.Qt.AlignRight
+        
+        return None
+    
+class ImageWidget (QtWidgets.QGraphicsView):
+    MAX_SCALING = 30.0
+    
+    def __init__ (self, parent):
+        QtWidgets.QGraphicsView.__init__ (self, parent)
+        self.pixmap = None
+        self.min_scaling = 1.0
+        self.dice_position = None
+        
+    def setImage (self, pixmap):
+        new_pixmap = self.pixmap == None
+
+        self.pixmap = pixmap
+        
+        scene = QtWidgets.QGraphicsScene (self)
+        if pixmap == None:
+            self.setScene (scene)
+            self.scale (1.0, 1.0)
+            self.min_scaling = 1.0
+            return
+        
+        scene.addPixmap (self.pixmap)
+        self.setScene (scene)
+        fx = self.width () / self.pixmap.width ()
+        fy = self.height () / self.pixmap.height ()
+        self.min_scaling = min ((fx, fy))
+        
+        if new_pixmap:
+            self.setScaling (self.min_scaling)
+        else:
+            self.changeScaling (1.0)
+            
+    def setDicePosition (self, pos):
+        print ("Dice position:", pos)
+        
+    def wheelEvent (self, event):
+        if event.modifiers () == QtCore.Qt.ControlModifier:
+            up_down = event.angleDelta ().y ()
+            left_right = event.angleDelta ().x ()
+             
+            if up_down != 0:
+                if up_down > 0:
+                    self.changeScaling (1.1, QtCore.QPoint (event.x (), event.y ()))
+
+                elif up_down < 0:
+                    self.changeScaling (1 / 1.1, QtCore.QPoint (event.x (), event.y ()))
+                
+                return
+            
+        return QtWidgets.QGraphicsView.wheelEvent (self, event)
+                
+    def changeScaling (self, rel_factor, keep_point = None):
+        t_old = self.transform ()
+        old_sf = 1.0 / t_old.m33 ()
+        
+        abs_sf = old_sf * rel_factor
+        
+        if abs_sf < self.min_scaling:
+            abs_sf = self.min_scaling
+            rel_factor = abs_sf / old_sf
+        elif abs_sf > ImageWidget.MAX_SCALING:
+            abs_sf = ImageWidget.MAX_SCALING
+            rel_factor = abs_sf / old_sf
+        
+        t = QtGui.QTransform (1.0, 0.0, 0.0,
+                              0.0, 1.0, 0.0,
+                              0.0, 0.0, 1.0/rel_factor)
+        
+        #print ("rel {} * old {} = {}".format (rel_factor, old_sf, rel_factor * old_sf))
+        self.setTransform (t, True)
+
+        # Folgendes funktioniert leider noch nicht. Es soll eigentlich die angegebene Position erhalten bleiben
+        # (Mausposition beim Zoom mit dem Mausrad)
+        if keep_point != None:
+            dx = keep_point.x () * (abs_sf - 1) - t_old.m31 ()
+            dy = keep_point.y () * (abs_sf - 1) - t_old.m32 ()
+            #print ("d: {}, {} (old: {}, {})".format (dx, dy, t_old.m31 (), t_old.m32 ()))
+
+            #self.scrollContentsBy (int (dx), int (dy))
+            
+    def setScaling (self, abs_factor):
+        if abs_factor < self.min_scaling:
+            abs_factor = self.min_scaling
+        elif abs_factor > ImageWidget.MAX_SCALING:
+            abs_factor = ImageWidget.MAX_SCALING
+
+        t = QtGui.QTransform (1.0, 0.0, 0.0,
+                              0.0, 1.0, 0.0,
+                              0.0, 0.0, 1.0 / abs_factor)
+        
+        self.setTransform (t, False)
+        
 class MainWindow (QtWidgets.QMainWindow):
     ui = Ui_main_window ()
     
     def __init__ (self):
+        self.image_base_dir = ""
+        self.img = None
         QtWidgets.QMainWindow.__init__ (self)
         self.ui = Ui_main_window ()
         self.ui.setupUi (self)
@@ -217,6 +332,13 @@ class MainWindow (QtWidgets.QMainWindow):
         
         self.image_model = ImagesModel ()
         self.ui.images_tbl.setModel (self.image_model)
+        self.ui.images_tbl.header ().setSectionResizeMode (QtWidgets.QHeaderView.ResizeToContents)
+        self.ui.images_tbl.selectionModel ().selectionChanged.connect (self.imageSelected)
+        
+        self.grid_layout_image = QtWidgets.QGridLayout (self.ui.image_container_w)
+        self.grid_layout_image.setContentsMargins (0, 0, 0, 0)
+        self.image_w = ImageWidget (self.ui.image_container_w)
+        self.grid_layout_image.addWidget(self.image_w, 0, 0, 1, 1)
     
     def snapImageClicked (self):
         print ("Snap Image Clicked")
@@ -229,6 +351,28 @@ class MainWindow (QtWidgets.QMainWindow):
         
     def updateImages (self):
         pass
+    
+    def updateImage (self):
+        self.image_w.setImage (self.img)
+        
+    def showImage (self, row):
+        if row < len (self.image_model.data_list):
+            fn = os.path.join (self.image_base_dir, self.image_model.data_list[row][0])
+            self.img = QtGui.QPixmap ()
+            self.img.load (fn)
+            #print ("Load:", fn)
+            self.updateImage ()
+            if len (self.image_model.data_list[row]) > 3:
+                self.image_w.setDicePosition ((self.image_model.data_list[row][2],
+                                               self.image_model.data_list[row][3]))
+    
+    def imageSelected (self, selected):
+        sel_list = selected.indexes ()
+        for sel in sel_list:
+            row = sel.row ()
+            if row < len (self.image_model.data_list):
+                self.showImage (row)
+                return
         
     def loadFile (self):
         options = QtWidgets.QFileDialog.Options ()
@@ -244,6 +388,7 @@ class MainWindow (QtWidgets.QMainWindow):
 
     def openFile (self, filename):
         data = []
+        self.image_base_dir = ""
 
         f = open (filename)
         s = ""
@@ -256,6 +401,12 @@ class MainWindow (QtWidgets.QMainWindow):
             logger.info ("{} Datensätze geladen.".format (len (data)))
             self.ui.statusbar.showMessage ("Loaded {} data sets from {}".format (len (data), os.path.basename (filename)), 10000)
             self.image_model.setDataList (data)
+            if len (data) >= 1:
+                self.image_base_dir = os.path.dirname (filename)
+                index1 = self.image_model.index (0, 0)
+                index2 = self.image_model.index (0, ImagesModel.COLUMNS-1)
+                self.ui.images_tbl.selectionModel ().select (QtCore.QItemSelection (index1, index2),
+                                                             QtCore.QItemSelectionModel.ClearAndSelect | QtCore.QItemSelectionModel.Current)
         except ValueError as e:
             logger.error ("Ladefehler: {}".format (e))
             self.image_model.setDataList ([])
