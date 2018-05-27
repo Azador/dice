0d20da8b91b869cc5f733078bdc142988b29ed27
Azador <github@lobo.mensa.uberspace.de>
2018-05-27 23:37:46 +0200

    First step to using tensorflow in the GUI.

--- dice.py
+++ dice.py
@@ -20,121 +20,46 @@ import config
 
 logger = logging.getLogger (__name__)
 
-def cnn_model_fn (features, labels, mode, params):
-  """Model function for CNN."""
-  # Input Layer
-  layers = params.get ("layers", [6348, 2048, 512, 128, 32, 2])
+# class Trainer:
+#     def __init__ (self, input_function):
+#         self.input_function = input_function
+#         
+#     def train (self):
+#         #self.training_data = loadData (self.training_fname)
+#         #print (self.training_data)
+#         
+#         image_small = tf.feature_column.numeric_column ('images_small',
+#                                                         #shape = (1, 92*69),
+#                                                         dtype=tf.float32)
+# 
+#         estimator = tf.estimator.Estimator(
+#                         model_fn=cnn_model_fn,
+#                         params={
+#                             "layers": [6348, 2048, 512, 128, 32, 2],
+#                             "learning_rate": 0.001,
+#                             "optimizer": tf.train.AdamOptimizer
+#                         },
+#                         model_dir="/home/harald/NoBackup/Würfel/ModelDir")
+# 
+#         #estimator = tf.estimator.DNNRegressor (
+#         #    feature_columns = [image_small],
+#         #    hidden_units=[8192, 2048, 512, 128, 32],
+#         #    label_dimension=2,
+#         #    model_dir="/home/harald/NoBackup/Würfel/ModelDir")
+#         
+#         while True:
+#             estimator.train (input_fn=self.input_function, steps=2)
+#             r = estimator.evaluate (input_fn=self.input_function, steps=1)
+#             print ("steps: {} loss: {} rmse: {}".format (r['global_step'], r['loss'], r['rmse']))
 
-  layer = tf.reshape (features["images_small"], [-1, 92 * 69])
+class DNNPosition:
+    def __init__ (self):
+#         image_small = tf.feature_column.numeric_column ('images_small',
+#                                                         #shape = (1, 92*69),
+#                                                         dtype=tf.float32)
 
-  for lsize in layers:
-      layer = tf.layers.dense (inputs=layer, units=lsize, activation=tf.nn.relu)
-
-  predictions = tf.squeeze (layer)
-
-  if mode == tf.estimator.ModeKeys.PREDICT:
-    return tf.estimator.EstimatorSpec (mode=mode, predictions={"position": predictions})
-
-  # Calculate Loss (for both TRAIN and EVAL modes)
-  average_loss = tf.losses.mean_squared_error (labels, predictions)
-
-  # Pre-made estimators use the total_loss instead of the average,
-  # so report total_loss for compatibility.
-  batch_size = tf.shape (labels)[0]
-  total_loss = tf.to_float (batch_size) * average_loss
-
-  if mode == tf.estimator.ModeKeys.TRAIN:
-    optimizer = params.get ("optimizer", tf.train.AdamOptimizer)
-    optimizer = optimizer (params.get ("learning_rate", None))
-    train_op = optimizer.minimize (loss=average_loss, global_step=tf.train.get_global_step ())
-
-    return tf.estimator.EstimatorSpec(mode=mode, loss=total_loss, train_op=train_op)
-
-  # In evaluation mode we will calculate evaluation metrics.
-  assert mode == tf.estimator.ModeKeys.EVAL
-
-  # Calculate root mean squared error
-  rmse = tf.metrics.root_mean_squared_error (labels, predictions)
-
-  # Add the rmse to the collection of evaluation metrics.
-  eval_metrics = {"rmse": rmse}
-
-  return tf.estimator.EstimatorSpec (
-      mode=mode,
-      # Report sum of error for compatibility with pre-made estimators
-      loss=total_loss,
-      eval_metric_ops=eval_metrics)
-
-def loadSmallImage (fname, *label):
-    #print ("###### fname:", fname)
-    image_string = tf.read_file (fname)
-    image = tf.image.decode_jpeg (image_string)
-    image = tf.image.convert_image_dtype (image, dtype=tf.float32)
-    image = tf.image.resize_images (image, [92, 69])
-    image = tf.image.rgb_to_grayscale (image)
-    image = tf.reshape (image, (6348, ))
-    
-    label = [ tf.cast (l, dtype=tf.float32) for l in label ]
-    #label = tf.cast (label, dtype=tf.float32)
-
-    #print ("###### img shape:", image.shape)
-    #print ("###### image:", image)
-    #print ("###### label:", label)
-    #print ("###### img size:", tf.size (image_resized).eval ())
-    return ( {"images_small": image }, *label )
-    #return ( {"image_small": image_resized, "image": image_decoded}, ( nr, pos ) )
-
-def loadData (fname):
-    f = open (fname)
-    s = ""
-    for l in f:
-        s += l
-    f.close ()
-
-    data = []
-    try:
-        data = json.loads (s)
-        logger.info ("{} Datensätze geladen.".format (len (data)))
-    except ValueError as e:
-        logger.error ("Ladefehler: {}".format (e))
-        data = []
-    
-    filenames = [ fn for fn, nr, posx, posy in data ]
-    numbers = [ nr for fn, nr, posx, posy in data ]
-    pos = [ (posx, posy) for fn, nr, posx, posy in data ]
-
-    dataset = tf.data.Dataset.from_tensor_slices ( (filenames, pos) )
-    dataset = dataset.map (loadSmallImage)
-    #dataset = dataset.shuffle (buffer_size=1)
-    dataset = dataset.batch (10)
-    dataset = dataset.repeat (10)
-    
-    iterator = dataset.make_one_shot_iterator()
-
-    # `features` is a dictionary in which each value is a batch of values for
-    # that feature; `labels` is a batch of labels.
-    features, labels = iterator.get_next()
-    return features, labels
-
-    print (dataset)
-    print (dataset)
-    
-    return ({'images_small': dataset}, labels)
-
-class Trainer:
-    def __init__ (self, training_fname):
-        self.training_fname = training_fname
-        
-    def train (self):
-        #self.training_data = loadData (self.training_fname)
-        #print (self.training_data)
-        
-        image_small = tf.feature_column.numeric_column ('images_small',
-                                                        #shape = (1, 92*69),
-                                                        dtype=tf.float32)
-
-        estimator = tf.estimator.Estimator(
-                        model_fn=cnn_model_fn,
+        self.estimator = tf.estimator.Estimator(
+                        model_fn=self.ModelFN,
                         params={
                             "layers": [6348, 2048, 512, 128, 32, 2],
                             "learning_rate": 0.001,
@@ -142,17 +67,75 @@ class Trainer:
                         },
                         model_dir="/home/harald/NoBackup/Würfel/ModelDir")
 
-        #estimator = tf.estimator.DNNRegressor (
-        #    feature_columns = [image_small],
-        #    hidden_units=[8192, 2048, 512, 128, 32],
-        #    label_dimension=2,
-        #    model_dir="/home/harald/NoBackup/Würfel/ModelDir")
-        
-        while True:
-            estimator.train (input_fn=lambda: loadData (self.training_fname), steps=2)
-            r = estimator.evaluate (input_fn=lambda: loadData (self.training_fname), steps=1)
+    @staticmethod
+    def ModelFN (features, labels, mode, params):
+      """Model function."""
+      # Input Layer
+      layers = params.get ("layers", [6348, 2048, 512, 128, 32, 2])
+    
+      layer = tf.reshape (features["images_small"], [-1, 92 * 69])
+    
+      for lsize in layers:
+          layer = tf.layers.dense (inputs=layer, units=lsize, activation=tf.nn.relu)
+    
+      predictions = tf.squeeze (layer)
+    
+      if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec (mode=mode, predictions={"position": predictions})
+    
+      # Calculate Loss (for both TRAIN and EVAL modes)
+      average_loss = tf.losses.mean_squared_error (labels, predictions)
+    
+      # Pre-made estimators use the total_loss instead of the average,
+      # so report total_loss for compatibility.
+      batch_size = tf.shape (labels)[0]
+      total_loss = tf.to_float (batch_size) * average_loss
+    
+      if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = params.get ("optimizer", tf.train.AdamOptimizer)
+        optimizer = optimizer (params.get ("learning_rate", None))
+        train_op = optimizer.minimize (loss=average_loss, global_step=tf.train.get_global_step ())
+    
+        return tf.estimator.EstimatorSpec(mode=mode, loss=total_loss, train_op=train_op)
+    
+      # In evaluation mode we will calculate evaluation metrics.
+      assert mode == tf.estimator.ModeKeys.EVAL
+    
+      # Calculate root mean squared error
+      rmse = tf.metrics.root_mean_squared_error (labels, predictions)
+    
+      # Add the rmse to the collection of evaluation metrics.
+      eval_metrics = {"rmse": rmse}
+    
+      return tf.estimator.EstimatorSpec (
+          mode=mode,
+          # Report sum of error for compatibility with pre-made estimators
+          loss=total_loss,
+          eval_metric_ops=eval_metrics)
+    
+    
+    def train (self, input_function):
+        for i in range (5):
+            self.estimator.train (input_fn=input_function, steps=2)
+            r = self.estimator.evaluate (input_fn=input_function, steps=1)
             print ("steps: {} loss: {} rmse: {}".format (r['global_step'], r['loss'], r['rmse']))
             
+    def eval (self, input_function):
+        return self.estimator.evaluate (input_fn=input_function, steps=1)
+        
+            
+class ImageEntry:
+    def __init__ (self, file_name, value = None, pos_x = None, pos_y = None):
+        self.file_name = file_name
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.value = value
+        
+    def hasPos (self):
+        return self.pos_x != None and self.pos_y != None
+    
+    def hasValue (self):
+        return self.value != None
 
 class ImagesModel (QtGui.QStandardItemModel):
     
@@ -160,14 +143,71 @@ class ImagesModel (QtGui.QStandardItemModel):
     
     def __init__ (self):
         QtGui.QStandardItemModel.__init__ (self, 0, 4)
-        self.setDataList([])
+        self.setDataList([], "")
         
-    def setDataList (self, data):
+    def setDataList (self, data, base_dir):
+        self.base_dir = base_dir
         self.beginResetModel ()
         self.data_list = data
         self.endResetModel ()
         self.setRowCount (len (data))
         self.dataChanged.emit (self.index (0,0), self.index (len (data)-1, 3))
+
+    @staticmethod
+    def loadSmallImage (fname, *label):
+        #print ("###### fname:", fname)
+        image_string = tf.read_file (fname)
+        image = tf.image.decode_jpeg (image_string)
+        image = tf.image.convert_image_dtype (image, dtype=tf.float32)
+        image = tf.image.resize_images (image, [92, 69])
+        image = tf.image.rgb_to_grayscale (image)
+        image = tf.reshape (image, (6348, ))
+        
+        label = [ tf.cast (l, dtype=tf.float32) for l in label ]
+        #label = tf.cast (label, dtype=tf.float32)
+    
+        #print ("###### img shape:", image.shape)
+        #print ("###### image:", image)
+        #print ("###### label:", label)
+        #print ("###### img size:", tf.size (image_resized).eval ())
+        return ( {"images_small": image }, *label )
+        #return ( {"image_small": image_resized, "image": image_decoded}, ( nr, pos ) )
+    
+    def getTrainingPosData (self):
+        filenames = [ os.path.join (self.base_dir, d.file_name) for d in self.data_list ]
+        numbers = [ d.value for d in self.data_list ]
+        pos = [ (d.pos_x, d.pos_y) for d in self.data_list ]
+    
+        dataset = tf.data.Dataset.from_tensor_slices ( (filenames, pos) )
+        dataset = dataset.map (ImagesModel.loadSmallImage)
+        #dataset = dataset.shuffle (buffer_size=1)
+        dataset = dataset.batch (10)
+        dataset = dataset.repeat (10)
+        
+        iterator = dataset.make_one_shot_iterator()
+    
+        # `features` is a dictionary in which each value is a batch of values for
+        # that feature; `labels` is a batch of labels.
+        features, labels = iterator.get_next()
+        return features, labels
+    
+    def getEvalPosData (self, row):
+        filenames = [ os.path.join (self.base_dir, self.data_list[row].file_name) ]
+        numbers = [ self.data_list[row].value ]
+        pos = [ (self.data_list[row].pos_x, self.data_list[row].pos_y) ]
+    
+        dataset = tf.data.Dataset.from_tensor_slices ( (filenames, pos) )
+        dataset = dataset.map (ImagesModel.loadSmallImage)
+        #dataset = dataset.shuffle (buffer_size=1)
+        dataset = dataset.batch (1)
+        dataset = dataset.repeat (1)
+        
+        iterator = dataset.make_one_shot_iterator()
+    
+        # `features` is a dictionary in which each value is a batch of values for
+        # that feature; `labels` is a batch of labels.
+        features, labels = iterator.get_next()
+        return features, labels
     
     def columnCount (self, parent_index):
         if parent_index == QtCore.QModelIndex ():
@@ -198,22 +238,25 @@ class ImagesModel (QtGui.QStandardItemModel):
             col = index.column ()
             if role == QtCore.Qt.DisplayRole:
                 if col == ImagesModel.IMAGE_NAME:
-                    return os.path.basename (self.data_list[row][0])
+                    return os.path.basename (self.data_list[row].file_name)
                 
                 elif col == ImagesModel.POS_X:
-                    if len (self.data_list[row]) >= 3:
-                        return "{}".format (self.data_list[row][2])
+                    if self.data_list[row].hasPos ():
+                        return "{}".format (self.data_list[row].pos_x)
                     else:
                         return "---"
                     
                 elif col == ImagesModel.POS_Y:
-                    if len (self.data_list[row]) >= 3:
-                        return "{}".format (self.data_list[row][3])
+                    if self.data_list[row].hasPos ():
+                        return "{}".format (self.data_list[row].pos_y)
                     else:
                         return "---"
                     
                 elif col == ImagesModel.VALUE:
-                    return "{}".format (self.data_list[row][1])
+                    if self.data_list[row].hasValue ():
+                        return "{}".format (self.data_list[row].value)
+                    else:
+                        return "---"
                 
             elif role == QtCore.Qt.TextAlignmentRole:
                 if col == ImagesModel.IMAGE_NAME:
@@ -362,12 +405,26 @@ class MainWindow (QtWidgets.QMainWindow):
         self.grid_layout_image.setContentsMargins (0, 0, 0, 0)
         self.image_w = ImageWidget (self.ui.image_container_w)
         self.grid_layout_image.addWidget(self.image_w, 0, 0, 1, 1)
+
+        self.dnn_position = DNNPosition ()
     
     def snapImageClicked (self):
         print ("Snap Image Clicked")
         
     def analyzeImageClicked (self):
         print ("Analyze Image Clicked")
+        
+        sel_list = self.ui.images_tbl.selectionModel ().selection ().indexes ()
+        row = None
+        
+        for s in sel_list:
+            if s.row () < len (self.image_model.data_list):
+                row = s.row ()
+                break
+        
+        if row != None:
+            r = self.dnn_position.eval (lambda: self.image_model.getEvalPosData (row))
+            print (r)
         
     def changeDataClicked (self):
         print ("Change Data Clicked")
@@ -380,7 +437,7 @@ class MainWindow (QtWidgets.QMainWindow):
         
     def showImage (self, row):
         if row < len (self.image_model.data_list):
-            fn = os.path.join (self.image_base_dir, self.image_model.data_list[row][0])
+            fn = os.path.join (self.image_base_dir, self.image_model.data_list[row].file_name)
             self.img = QtGui.QPixmap ()
             self.img.load (fn)
             if self.img.width () < self.img.height ():
@@ -393,14 +450,14 @@ class MainWindow (QtWidgets.QMainWindow):
             self.ui.dice_position_y_sb.setMaximum (self.img.height ())
             
             self.updateImage ()
-            if len (self.image_model.data_list[row]) > 1:
-                self.ui.dice_value_sb.setValue (self.image_model.data_list[row][1])
+            if self.image_model.data_list[row].hasValue ():
+                self.ui.dice_value_sb.setValue (self.image_model.data_list[row].value)
 
-            if len (self.image_model.data_list[row]) > 3:
-                self.image_w.setDicePosition ((self.image_model.data_list[row][2],
-                                               self.image_model.data_list[row][3]))
-                self.ui.dice_position_x_sb.setValue (self.image_model.data_list[row][2])
-                self.ui.dice_position_y_sb.setValue (self.image_model.data_list[row][3])
+            if self.image_model.data_list[row].hasPos ():
+                self.image_w.setDicePosition ((self.image_model.data_list[row].pos_x,
+                                               self.image_model.data_list[row].pos_y))
+                self.ui.dice_position_x_sb.setValue (self.image_model.data_list[row].pos_x)
+                self.ui.dice_position_y_sb.setValue (self.image_model.data_list[row].pos_y)
             
     
     def imageSelected (self, selected):
@@ -435,9 +492,10 @@ class MainWindow (QtWidgets.QMainWindow):
         
         try:
             data = json.loads (s)
+            data = [ ImageEntry (*d) for d in data ]
             logger.info ("{} Datensätze geladen.".format (len (data)))
             self.ui.statusbar.showMessage ("Loaded {} data sets from {}".format (len (data), os.path.basename (filename)), 10000)
-            self.image_model.setDataList (data)
+            self.image_model.setDataList (data, os.path.dirname (filename))
             if len (data) >= 1:
                 self.image_base_dir = os.path.dirname (filename)
                 index1 = self.image_model.index (0, 0)
